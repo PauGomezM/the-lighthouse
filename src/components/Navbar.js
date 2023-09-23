62fe698e6d62074cd51eaf236c3d7d3243ff4d80
paugomezm <pgm5060@gmail.com>
2023-09-23 15:11:14 +0200

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './img/Lighthouse.jpg';
 
 function Navbar() {
     return (
         <div className="navbar">
-            <h1 className="navbar-home" >The Lighthouse</h1>
+            <h1 className="navbar-home" ><img src="img/Lighthouse.jpg"></img>The Lighthouse</h1>
         </div>
     );
 }
