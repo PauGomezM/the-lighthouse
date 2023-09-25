ca6c1ea723d095b66679c5bb5c366ce234a8b8fe
paugomezm <pgm5060@gmail.com>
2023-09-25 16:05:53 +0200

    update

--- src/components/Pages/AboutBooks.js
+++ src/components/Pages/AboutBooks.js
@@ -25,13 +25,13 @@ function AboutBooks() {
     return (
         <div className="bg-gray-900 p-2">
             <div className="">
-                <input placeholder="Search a book review..." value={bookName} onChange={handleBookTermChange} />
-                <button onClick={searchBooks}>Search</button>
+                <input className="bg-cyan-900 p-1 w-full border rounded-md border-cyan-500" placeholder="Search a book review..." value={bookName} onChange={handleBookTermChange} />
+                <button className="px-2 py-1 text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm mt-2" onClick={searchBooks}>Search</button>
             </div>
             <div className="">
                 {books.map((book) => (
-                    <div key={book.id} className="">
-                        <img src={book.volumeInfo.imageLinks?.thumbnail} alt={`Thumbnail for ${book.volumeInfo.title}`} />
+                    <div key={book.id} className="flex flex-col w-full border border-cyan-300 p-2 rounded-md mt-3">
+                        <img className="w-20 h-auto" src={book.volumeInfo.imageLinks?.thumbnail} alt={`Thumbnail for ${book.volumeInfo.title}`} />
                         <h3>{book.volumeInfo.title}</h3>
                         <p>Author: {book.volumeInfo.authors}</p>
                     </div>
--- src/components/Pages/Quotes.js
+++ src/components/Pages/Quotes.js
@@ -20,20 +20,31 @@ function Quotes() {
 
     };
 
-    const renderedQuotesReviews = quoteCreated.map((quote, index) => <p className="" key={index}>{quote}</p>);
+    const handleDeleteQuote = (index) => {
+        const updatedQuotes = [...quoteCreated];
+        updatedQuotes.splice(index, 1);
+        setStoreQuote(updatedQuotes);
+    }
+
+    const renderedQuotesReviews = quoteCreated.map((quote, index) =>
+        <div className="flex items-center justify-center ">
+            <p className="flex flex-col bg-cyan-900 border rounded-xl border-cyan-600 text-cyan-100 px-2 py-1 mt-2 break-words nearly-full" key={index}>{quote}</p>
+            <button className=" bg-red-950 ml-1 text-red-50 px-3 py-1 rounded-full border border-red-600" onClick={handleDeleteQuote}>X</button>
+        </div> 
+    );
 
     const maxCharacterCount = 100;
-    
+
     return (
-        <div className="bg-cyan-950 flex flex-col p-3">
+        <div className="bg-cyan-950 flex flex-col p-3 max-w-full">
             <form onSubmit={handleFormSubmit}>
                 <input className="bg-cyan-900 p-1 w-full border rounded-md border-cyan-500" type="text" placeholder="Type your quote..." value={quoteTyped} onChange={handleChange} maxLength={maxCharacterCount} />
             </form>
             <div className="flex flex-row items-center justify-between p-1">
                 <button className="px-2 py-1 text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm" onClick={handleFormSubmit}>Add quote</button>
-                <p>{maxCharacterCount - quoteTyped.length}</p>
+                <p className="text-gray-400">{maxCharacterCount - quoteTyped.length}</p>
             </div>
-            <div className="">{renderedQuotesReviews}</div>
+            <div className="p-2">{renderedQuotesReviews}</div>
         </div>
     );
 }
