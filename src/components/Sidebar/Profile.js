8edab918ea23235a163d509083709d65b84162d0
paugomezm <pgm5060@gmail.com>
2023-09-27 22:41:41 +0200

    added - add book review - section

--- src/components/Sidebar/Profile.js
+++ src/components/Sidebar/Profile.js
@@ -1,9 +1,9 @@
 function Profile() {
     return (
         <div className="bg-cyan-950 flex flex-col p-3 mr-2 border border-cyan-500 rounded-md">
-            <h2 className="bg-cyan-950 text-xl font-extrabold gradient-text2">PauGM</h2>
-            <p className="bg-cyan-950 text-gray-400">22 yo, male...</p>
-            <p className="bg-cyan-950 text-gray-400">Interests: chess, web development, psycology books...</p>
+            <h2 className="gradient-text1 bg-cyan-950 text-xl font-extrabold">Pau Gómez M</h2>
+            <p className="bg-cyan-950 text-teal-500">22 yo, male...</p>
+            <p className="bg-cyan-950 text-teal-500">Interests: chess, web development, psycology books...</p>
         </div>
     );
 }
